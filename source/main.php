cb4d1360b1191dad333ae228ee8e5a47de188763
Damon V. Caskey <dc@caskeys.com>
2018-02-02 13:59:05 -0500

    Update non-submodule class references.

--- source/main.php
+++ source/main.php
@@ -3,13 +3,13 @@
 	require_once(__DIR__.'/settings.php');
 	// Nahoni require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/session.php');			// Session class.
 	require(__DIR__.'/navigation.php');
-	// require($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/record_navigation/main.php');	// Record navigation.
-	// require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/sorting/main.php'); 		// Page cache.
-	// require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/cache/main.php'); 		// Page cache.
+	require(__DIR__.'/source/dc/record_navigation/main.php');	// Record navigation.
+	require(__DIR__.'/source/dc/sorting/main.php'); 		// Record sorting.
+	require(__DIR__.'/source/dc/cache/main.php'); 		// Page cache.
 	require(__DIR__.'/data_main.php');
 	// Yukon require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/database/main.php'); 	// Database class.
 	// Stoeckl require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/access/main.php');
-	// require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/url_query/main.php'); 	// URL builder (to include variables).
+	require(__DIR__.'/source/dc/url_query/main.php'); 	// URL builder (to include variables).
 
 		
 	// Replace default session handler.
