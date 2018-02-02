4a099563ad2238f2e97a27252613ccbf51791148
Damon V. Caskey <dc@caskeys.com>
2018-02-02 13:51:44 -0500

    Add sorting class files.

--- source/main.php
+++ source/main.php
@@ -3,8 +3,8 @@
 	require_once(__DIR__.'/settings.php');
 	// Nahoni require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/session.php');			// Session class.
 	require(__DIR__.'/navigation.php');
-	require($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/record_navigation/main.php');	// Record navigation.
-	require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/sorting/main.php'); 		// Page cache.
+	// require($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/record_navigation/main.php');	// Record navigation.
+	// require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/sorting/main.php'); 		// Page cache.
 	require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/cache/main.php'); 		// Page cache.
 	require(__DIR__.'/data_main.php');
 	// Yukon require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/database/main.php'); 	// Database class.
