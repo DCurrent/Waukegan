0c2def7cde9069beb3233e6c474f6ce2e5b7c0dc
Damon V. Caskey <dc@caskeys.com>
2018-02-02 13:44:32 -0500

    Remove database class requirement.

--- source/main.php
+++ source/main.php
@@ -1,13 +1,13 @@
 <?php
 
 	require_once(__DIR__.'/settings.php');
-	//Nahoni require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/session.php');			// Session class.
+	// Nahoni require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/session.php');			// Session class.
 	require(__DIR__.'/navigation.php');
 	require($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/record_navigation/main.php');	// Record navigation.
 	require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/sorting/main.php'); 		// Page cache.
 	require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/cache/main.php'); 		// Page cache.
 	require(__DIR__.'/data_main.php');
-	require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/database/main.php'); 	// Database class.
+	// Yukon require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/database/main.php'); 	// Database class.
 	require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/access/main.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/php/classes/url_query/main.php'); 	// URL builder (to include variables).
 
