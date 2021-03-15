9b5ed381ed6ae8780c4fb61f562346775007d572
Caskey, Damon V <dc@caskeys.com>
2021-03-15 02:36:37 -0400

    Remove google analytics code.

--- ticket.php
+++ ticket.php
@@ -975,13 +975,6 @@
             <?php echo $obj_navigation_main->get_markup_footer(); ?>
         </div><!--container-->        
     <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-40196994-1', 'uky.edu');
-  ga('send', 'pageview');
   
   $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
